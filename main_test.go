f76b2a099aa9836f7f58edf144770b4a1ed9b0e8
Jef Spaleta <jspaleta@gmail.com>
2020-08-25 19:47:12 -0800

    add test for check annotation keyspace based topic override

--- main_test.go
+++ main_test.go
@@ -25,9 +25,59 @@ func SetUnexportedField(field reflect.Value, value interface{}) {
 		Set(reflect.ValueOf(value))
 }
 
+func TestTopicAnnotation(t *testing.T) {
+	assert := assert.New(t)
+	file, _ := ioutil.TempFile(os.TempDir(), "sensu-kafka-handler-")
+	defer func() {
+		_ = os.Remove(file.Name())
+	}()
+
+	event := corev2.FixtureEvent("entity1", "check1")
+	val := `alt-topic`
+	m := make(map[string]string)
+	m["sensu.io/plugins/sensu-kafa-handler/config/topic"] = val
+	event.Check.Annotations = m
+	eventJSON, _ := json.Marshal(event)
+	_, err := file.WriteString(string(eventJSON))
+	require.NoError(t, err)
+	require.NoError(t, file.Sync())
+	_, err = file.Seek(0, 0)
+	require.NoError(t, err)
+	os.Stdin = file
+
+	// Using docker compose to startup kafka server and prepare sensu-events topic
+	host := "localhost:9092"
+	oldArgs := os.Args
+	os.Args = []string{"kafka-handler", "-H", host, "-t", "undefined-events"}
+	defer func() { os.Args = oldArgs }()
+
+	var exitStatus int
+	exitStatus = 0
+	mockExit := func(i int) {
+		exitStatus = i
+	}
+
+	handler := sensu.NewGoHandler(&plugin.PluginConfig, options, checkArgs, executeHandler)
+	field := reflect.ValueOf(handler).Elem().FieldByName("exitFunction")
+	SetUnexportedField(field, mockExit)
+	c1 := make(chan string, 1)
+	go func() {
+		handler.Execute()
+		c1 <- "execute is done"
+	}()
+	select {
+	case <-c1:
+		assert.Zero(exitStatus)
+	case <-time.After(10 * time.Second):
+		fmt.Println("Error: timeout reached in test")
+		assert.True(false)
+	}
+
+}
+
 func TestExecute(t *testing.T) {
 	assert := assert.New(t)
-	file, _ := ioutil.TempFile(os.TempDir(), "sensu-handler-influx-db-")
+	file, _ := ioutil.TempFile(os.TempDir(), "sensu-kafka-handler-")
 	defer func() {
 		_ = os.Remove(file.Name())
 	}()
@@ -50,7 +100,7 @@ func TestExecute(t *testing.T) {
 	defer func() { os.Args = oldArgs }()
 
 	var exitStatus int
-	exitStatus = 1
+	exitStatus = 0
 	mockExit := func(i int) {
 		exitStatus = i
 	}
@@ -64,12 +114,11 @@ func TestExecute(t *testing.T) {
 		c1 <- "execute is done"
 	}()
 	select {
-	case res := <-c1:
-		fmt.Println(res)
+	case <-c1:
 		assert.Zero(exitStatus)
 	case <-time.After(10 * time.Second):
-		fmt.Println("timeout 10")
-		assert.NotZero(exitStatus)
+		fmt.Println("Error: timeout reached in test")
+		assert.True(false)
 	}
 
 }
