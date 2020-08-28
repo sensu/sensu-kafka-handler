38f4824b811a3e999faa0a2464bab8636e0d7ef4
Jef Spaleta <jspaleta@gmail.com>
2020-08-28 15:34:08 -0800

    make linter happy

--- main_test.go
+++ main_test.go
@@ -128,9 +128,9 @@ func TestExecute(t *testing.T) {
 	topic := "sensu-events"
 	partition := 0
 
-	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
 
-	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	err = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 
 	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
 	b := make([]byte, 10e3)            // 10KB max per message
